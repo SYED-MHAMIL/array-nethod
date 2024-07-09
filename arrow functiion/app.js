d7c4afe3d5663131ce01a3be8cc41c0b2aa9706e
syed mhamil <160579726+SYED-MHAMIL@users.noreply.github.com>
2024-07-09 04:58:08 -0700

    Add files via upload

---
+++ arrow functiion/app.js
@@ -0,0 +1,48 @@
+// function greet(){
+//        console.log("welcome");
+// }
+// greet()
+
+
+   // let greet=function(){
+   //  console.log("welcome");
+
+   // }
+// greet();
+// greet();
+
+
+// let greet=()=>{
+//    console.log("welcome");
+//   }
+// greet();
+
+
+
+// let greet=()=>{return "welcome"}
+// console.log(greet());
+
+
+
+
+// single line code hotu  "  curlyy bracket or return dono remove krsktay han"
+
+// let greet=()=> "welcome"
+// console.log(greet());
+
+
+
+
+
+// set time out
+
+
+   //   setTimeout(()=>{document.write("mhamil")},2000);
+      let a=4;
+      b=4;
+     let sum=(a,b)=> a-b;
+     console.log(sum(50,56));
+
+
+
+     
